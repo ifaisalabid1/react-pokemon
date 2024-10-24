0457885b377a3c61b87e3333c0aaaf0eba7c2975
Faisal Abid <ifaisalabid1@gmail.com>
2024-10-24 23:59:05 +0530

    Add stats of pokemon

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -1,16 +1,31 @@
 const PokeCard = ({ currPokemon }: any) => {
   return (
     <div className='shadow-xl card bg-base-100'>
-      <figure className='px-10 pt-10'>
+      <figure>
         <img
           src={currPokemon.sprites.other.dream_world.front_default}
-          alt='Shoes'
-          className='rounded-xl'
+          alt={currPokemon.name}
         />
       </figure>
-      <div className='items-center text-center card-body'>
-        <h2 className='card-title'>{currPokemon.name}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div className='card-body'>
+        <h2 className='capitalize card-title'>{currPokemon.name}</h2>
+
+        <div className='mt-6'>
+          {currPokemon.stats.map((pokemonStat: any) => (
+            <div
+              className='flex items-center gap-4 text-sm'
+              key={pokemonStat.stat.name}
+            >
+              <p className='my-2 capitalize w-[30%]'>{pokemonStat.stat.name}</p>
+              <p className='w-[10%]'>{pokemonStat.base_stat}</p>
+              <progress
+                className='progress progress-primary w-[60%]'
+                value={pokemonStat.base_stat}
+                max='100'
+              ></progress>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
